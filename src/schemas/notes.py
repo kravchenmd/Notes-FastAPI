3c7acf4b6c04973258c3b1bb8aa5b7178f53c2fc
Michail Kravchenko <66253910+kravchenmd@users.noreply.github.com>
2022-10-23 16:49:08 +0200

    Add schemas for Notes

---
+++ src/schemas/notes.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from pydantic import BaseModel, Field
+
+
+class NoteBase(BaseModel):
+    title: str = Field(max_length=50)
+    description: str = Field(max_length=150, description="Description of the note")
+
+
+class NoteUpdate(NoteBase):
+    done: bool
+
+
+class NoteResponse(NoteBase):
+    id: int
+    done: bool
+    created: datetime
+
+    class Config:
+        orm_mode = True
