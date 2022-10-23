5a7d8da3c3cadbb91dde630d64ed15f107ace310
Michail Kravchenko <66253910+kravchenmd@users.noreply.github.com>
2022-10-23 17:41:12 +0200

    Add and edit functions for notes

--- src/repository/notes.py
+++ src/repository/notes.py
@@ -1,10 +1,10 @@
 from sqlalchemy.orm import Session
 from src.models import Note
-from src.schemas.notes import NoteBase, NoteUpdate
+from src.schemas.notes import NoteBase, NoteUpdate, NoteDone
 
 
-async def get_all_notes(db: Session):
-    all_notes = db.query(Note).all()
+async def get_all_notes(db: Session, skip: int, limit: int):
+    all_notes = db.query(Note).offset(skip).limit(limit).all()
     return all_notes
 
 
@@ -21,12 +21,27 @@ async def create_note(db: Session, note: NoteBase):
     return new_note
 
 
-async def update_note(db: Session, note_id: int, note_update: NoteUpdate):
+async def update_note(db: Session, note_id: int, new_note: NoteUpdate):
     note = db.query(Note).filter(Note.id == note_id).first()
     if note:
-        pass
+        note.title = new_note.title
+        note.description = new_note.description
+        note.done = new_note.done
+        db.commit()
     return note
 
 
 async def delete_note(db: Session, note_id: int):
-    return {}
+    note = db.query(Note).filter(Note.id == note_id).first()
+    if note:
+        db.delete(note)
+        db.commit()
+    return note
+
+
+async def done_note(db: Session, note_id: int, new_note: NoteDone):
+    note = db.query(Note).filter(Note.id == note_id).first()
+    if note:
+        note.done = new_note.done
+        db.commit()
+    return note
